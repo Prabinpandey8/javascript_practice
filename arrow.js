fd867b3c8af509e4359939b27e804128a7e62522
Prabin Pandey <163797181+Prabinpandey8@users.noreply.github.com>
2026-04-20 21:27:53 +0545

    arrow

---
+++ arrow.js
@@ -0,0 +1,32 @@
+// Q-no 1
+const add = (a,b) => {
+    return (a+b)
+}
+const result=add(5,6)
+console.log(result);
+
+
+// Q-no 2
+const isEven = (a) => {
+    return a % 2 ==0;
+}
+console.log (isEven(10));
+
+// q-no 3
+const sqr = a => {
+    return (a * a)
+}
+console.log(sqr(6));
+
+// q-no 4
+const greet = (name) => {
+    return ("Hello"+name)
+}
+console.log(greet(" Prabin"))
+
+
+// q-no 5
+const len = (name) => {
+    return name.length;
+}
+console.log(len("prabin"))
